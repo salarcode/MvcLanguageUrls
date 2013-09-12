d820200895d0667b11b69b1f6d84d3dbfb66905e
salar2k <salar2k@gmail.com>
2013-09-12 08:55:52 +0430

    Making compatible with bundled urls.

--- MvcLanguageUrls/RedirectToLozalizedRoute.cs
+++ MvcLanguageUrls/RedirectToLozalizedRoute.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MvcLanguageUrls
@@ -18,7 +19,7 @@ namespace MvcLanguageUrls
 		/// <summary>
 		/// 
 		/// </summary>
-		public RedirectToLozalizedRoute(bool useCurrentCultureLangauge,string defaultLanguage)
+		public RedirectToLozalizedRoute(bool useCurrentCultureLangauge, string defaultLanguage)
 			: base(ControllerActionId, new MvcRouteHandler())
 		{
 			_useCurrentCultureLangauge = useCurrentCultureLangauge;
@@ -27,7 +28,7 @@ namespace MvcLanguageUrls
 			{
 				_defaultLanguage = MvcUrlExtension.GetCultureTwoDigit();
 			}
-			Defaults = new RouteValueDictionary(new {controller = "Home", action = "Index", id = UrlParameter.Optional});
+			Defaults = new RouteValueDictionary(new { controller = "Home", action = "Index", id = UrlParameter.Optional });
 		}
 
 		public override RouteData GetRouteData(HttpContextBase httpContext)
@@ -58,6 +59,13 @@ namespace MvcLanguageUrls
 			var originalUrl = httpContext.Request.Url.PathAndQuery;
 			if (originalUrl[0] != '/')
 				originalUrl = '/' + originalUrl;
+
+			// is bundle?
+			if (IsBundledUrl(originalUrl))
+			{
+				return;
+			}
+
 			var redirectUrl = string.Format("/{0}{1}", language, originalUrl);
 
 			httpContext.Response.Status = "302 Redirect to localized version";
@@ -65,5 +73,55 @@ namespace MvcLanguageUrls
 			httpContext.Response.AddHeader("Location", redirectUrl);
 			httpContext.Response.End();
 		}
+
+		private bool IsBundledUrl(string url)
+		{
+			if (string.IsNullOrEmpty(url))
+				return true;
+			url = MakeCompatibleActionUrl(url);
+
+			foreach (var bundle in BundleTable.Bundles)
+			{
+				var path = MakeCompatibleActionUrl(bundle.Path);
+				if (url == path)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Makes bundled urls compatible with relative urls
+		/// </summary>
+		private static string MakeCompatibleActionUrl(string url)
+		{
+			if (string.IsNullOrEmpty(url))
+				return url;
+			url = url.ToLower();
+			var qindex = url.IndexOf("?", StringComparison.Ordinal);
+			if (qindex > -1)
+			{
+				url = url.Substring(0, qindex);
+			}
+			else
+			{
+				var qhash = url.IndexOf("#", StringComparison.Ordinal);
+				if (qhash > -1)
+				{
+					url = url.Substring(0, qhash);
+				}
+			}
+			if (url[0] == '~')
+				url = url.Remove(0, 1);
+			if (url[0] != '/')
+				url = '/' + url;
+
+			if (url[url.Length - 1] != '/')
+				url = url + '/';
+			return url;
+		}
 	}
+
+
 }
